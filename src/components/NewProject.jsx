484933eea4a4bc588a66b8ee5ef3da1f66125cfa
Sahil <sahil.kumar5729@gmail.com>
2024-02-13 15:52:07 +0530

    Add New Project component

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,24 @@
+export default function NewProject () {
+    return (
+        <div>
+            <menu>
+                <li><button>Cancel</button></li>
+                <li><button>Save</button></li>
+            </menu>
+            <div>
+                <p>
+                    <label>Title</label>
+                    <input type="text" />
+                </p>
+                <p>
+                    <label>Title</label>
+                    <textarea cols="30" rows="10"></textarea>
+                </p>
+                <p>
+                    <label >Title</label>
+                    <input type="text" />
+                </p>
+            </div>
+        </div>
+    );
+}
