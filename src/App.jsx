9f970194eb1d029d26cb21b9fbee85d5d851dad1
Sahil <sahil.kumar5729@gmail.com>
2024-02-15 18:48:15 +0530

    Add Sidebar, main content, and state management for projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
+import { useState } from "react";
+import DefaultScreen from "./components/DefaultScreen";
+import NewProject from "./components/NewProject";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectProject: undefined,
+    projects: []
+  });
+
+  function handleProjectState() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(project) {
+    setProjectsState((prevState) => {
+      const newProject = {
+        ...project,
+        id: Math.random()
+      };
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      }
+    })
+  }
+
+  console.log(projectsState);
+  
+  let content;
+
+  if (projectsState.selectedProjectId === undefined)
+    content = <DefaultScreen onClickAddProject={handleProjectState} />;
+  else if (projectsState.selectedProjectId === null)
+    content = <NewProject onAddNewProject={handleAddProject} />
+
   return (
-    <main>
-      <ProjectsSidebar />
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onClickAddProject={handleProjectState} />
+      {content}
     </main>
   );
 }
