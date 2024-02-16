2b053a65eb8354047983a9c23e34b667c0241e6a
Sahil <sahil.kumar5729@gmail.com>
2024-02-16 20:51:09 +0530

    Add style to projects list

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,6 +1,9 @@
 import Button from "./Button.jsx";
 
 export default function ProjectsSidebar({ onClickAddProject, projects }) {
+  let buttonClasses =
+    "w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800";
+
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -9,10 +12,12 @@ export default function ProjectsSidebar({ onClickAddProject, projects }) {
       <div>
         <Button onClick={onClickAddProject}>+ Add Project</Button>
       </div>
-      <ul>
-        {projects.map((project) => <li key={project.id}>
-          <button>{project.title}</button>
-        </li>)}
+      <ul className="mt-8">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className={buttonClasses}>{project.title}</button>
+          </li>
+        ))}
       </ul>
     </aside>
   );
