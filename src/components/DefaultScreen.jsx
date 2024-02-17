e07659e4f078cd7801f355f7d0d6c684bed8d4e8
Sahil <sahil.kumar5729@gmail.com>
2024-02-17 21:29:45 +0530

    Add onClickAddProject prop to DefaultScreen component

--- src/components/DefaultScreen.jsx
+++ src/components/DefaultScreen.jsx
@@ -1,7 +1,7 @@
 import noProjectImage from "../assets/no-projects.png";
 import Button from "./Button.jsx";
 
-export default function DefaultScreen() {
+export default function DefaultScreen({onClickAddProject}) {
   return (
     <div className="mt-24 text-center w-2/3">
       <img
@@ -14,7 +14,7 @@ export default function DefaultScreen() {
       </h2>
       <p className="text-stone-400 mb-4">Select a project or get started with a new one</p>
       <p className="mt-8">
-        <Button>Create new project</Button>
+        <Button onClick={onClickAddProject}>Create new project</Button>
       </p>
     </div>
   );
